4889016ba891cdbef327be0e7447baa428a87534
Cherry <180835910@qq.com>
2021-01-29 19:33:32 +0800

    fix sql

--- src/main/java/com/icbms/iot/mapper/GatewayDeviceMapMapper.java
+++ src/main/java/com/icbms/iot/mapper/GatewayDeviceMapMapper.java
@@ -15,6 +15,6 @@ public interface GatewayDeviceMapMapper {
     @Select("select device_id from gateway_device_map where gym_id = #{gymId} AND project_id = #{projectId} AND gateway_id = #{gatewayId}")
     List<String> findDeviceSns(int gymId, String projectId, String gatewayId);
 
-    @Select("select gateway_id as gatewayId device_id as deviceSn from where gym_id = #{gymId} AND project_id = #{projectId}")
+    @Select("select gateway_id as gatewayId device_id as deviceSn from gateway_device_map where gym_id = #{gymId} AND project_id = #{projectId}")
     List<GatewayDeviceIdMapDto> findDeviceSns(int gymId, String projectId);
 }
