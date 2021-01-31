042959ef337ffd67bbb44442191400935815a7c0
jaytube <tuxiaoyue@126.com>
2021-01-31 15:11:51 +0800

    fix gateway issue

--- src/main/java/com/icbms/iot/inbound/service/impl/GatewaySingleRunner.java
+++ src/main/java/com/icbms/iot/inbound/service/impl/GatewaySingleRunner.java
@@ -33,22 +33,34 @@ public class GatewaySingleRunner implements GatewayRunner {
 
         logger.info("线程:" + Thread.currentThread().getName()+" 开启单个网关轮询...");
         while(!gateway.isStopped()) {
-            CommonResponse resp = loRaCommandService.startRoundRobin(gateway.getIp());
-            gateway.setFinished(false);
-            logger.info("开启轮询网关" + gateway.getId() + ", ip: " + gateway.getIp() + ", 响应：" + resp.getData());
-            long timeCost = 0;
-            long start = System.currentTimeMillis();
-            while(!gateway.isFinished() && timeCost < MAX_WAITING_TIME) {
-                timeCost = (System.currentTimeMillis() - start) / 1000;
-                gateway = gatewayKeeper.getById(gatewayId);
-                continue;
-            }
-            logger.info("网关" + + gateway.getId() + ", ip: " + gateway.getIp() + "轮询结束, 本次轮询花费时间:" + timeCost + "秒！");
             try {
-                TimeUnit.SECONDS.sleep(1);
-            } catch (InterruptedException e) {
+                processRoundRobin(gateway, gatewayId);
+                gateway = gatewayKeeper.getById(gatewayId);
+            }catch (Exception e) {
+                logger.error("SINGLE RUNNER ERROR: {}", e);
             }
+        }
+    }
+
+    private void processRoundRobin(GatewayDto gateway, int gatewayId) {
+        CommonResponse resp = loRaCommandService.startRoundRobin(gateway.getIp());
+        gateway.setFinished(false);
+        if(resp == null || resp.getCode() != 200)
+            return;
+
+        logger.info("开启轮询网关" + gateway.getId() + ", ip: " + gateway.getIp() + ", 响应：" + resp.getData());
+        long timeCost = 0;
+        long start = System.currentTimeMillis();
+        while (!gateway.isFinished() && timeCost < MAX_WAITING_TIME) {
+            timeCost = (System.currentTimeMillis() - start) / 1000;
             gateway = gatewayKeeper.getById(gatewayId);
+            continue;
+        }
+        logger.info("网关" + +gateway.getId() + ", ip: " + gateway.getIp() + "轮询结束, 本次轮询花费时间:" + timeCost + "秒！");
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            logger.info("sleep interrupted...");
         }
     }
 
