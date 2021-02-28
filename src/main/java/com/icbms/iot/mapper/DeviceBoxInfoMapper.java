5ba8a55e20fbecb96055876bb348410f5fc792a0
jaytube <tuxiaoyue@126.com>
2021-02-28 22:58:09 +0800

    fix an issue

--- src/main/java/com/icbms/iot/mapper/DeviceBoxInfoMapper.java
+++ src/main/java/com/icbms/iot/mapper/DeviceBoxInfoMapper.java
@@ -11,12 +11,13 @@ public interface DeviceBoxInfoMapper {
     @Select("select * from device_box_info where device_box_num = #{deviceBoxNum} AND project_id = #{projectId}")
     List<DeviceBoxInfo> findByDeviceBoxNumAndProjectId(String deviceBoxNum, String projectId);
 
-    @Select("<script>" +
-            "select * from device_box_info where project_id in " +
-            "<foreach item='item' index='index' collection='projectIds' open='(' separator=',' close=')'>" +
-            "#{item}" +
-            "</foreach>" +
-            "</script>")
+    @Select({"<script>",
+            "select * from device_box_info where project_id in (",
+            "<foreach item='item' index='index' collection='list' separator=','>",
+            "#{item}",
+            "</foreach>",
+            ")",
+            "</script>"})
     List<DeviceBoxInfo> findByProjectIdList(List<String> projectIds);
 
     @Delete("delete from device_box_info where project_id = #{projectId}")
